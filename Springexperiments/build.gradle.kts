eef8a268d456fc8e988bc86f025a3915c0655138
Setahero <setahero@gmail.com>
2023-06-07 12:51:34 +0200

    Added in-memory database

--- Springexperiments/build.gradle.kts
+++ Springexperiments/build.gradle.kts
@@ -16,7 +16,11 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.data:spring-data-jdbc")
+
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("com.h2database:h2")
 }
 
 tasks.withType<Test> {
