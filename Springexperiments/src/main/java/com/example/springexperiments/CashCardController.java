dc13b1e6fbf6b014439aa9edd0babcc730f94312
Setahero <setahero@gmail.com>
2023-06-07 16:24:04 +0200

    Finished Paging

--- Springexperiments/src/main/java/com/example/springexperiments/CashCardController.java
+++ Springexperiments/src/main/java/com/example/springexperiments/CashCardController.java
@@ -4,6 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+
 import java.net.URI;
 import java.util.Optional;
 
@@ -37,5 +43,17 @@ public class CashCardController {
                 .toUri();
     //    Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
         return ResponseEntity.created(location).build();
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Iterable<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
+        return ResponseEntity.ok(page.getContent());
     }
 }
